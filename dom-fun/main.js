db0ae64c6dc470c0e9dea6da9d8803188718aded
aw0lf <adrianna,tycner@yahoo.com>
2018-05-05 20:03:41 +0200

    insert Row

--- dom-fun/main.js
+++ dom-fun/main.js
@@ -41,3 +41,15 @@ function getAttributes(){
     var attributes="href: "+href+", hreflang: "+hreflang+", rel: "+ rel+", target: "+ target+", type: "+type;
     alert(attributes);
 }
+/*
+exercise 5
+*/
+var table=document.getElementById("sampleTable");
+function insert_Row(){
+    var row=table.insertRow(0);
+    var cell1=row.insertCell(0);
+    var cell2=row.insertCell(1);
+    cell1.innerHTML="New";
+    cell2.innerHTML="New2";
+    
+}
